1997e09a5bc9e21538dc0b0805a4261aad128f80
beba2123 <antenehbeba@gmail.com>
2023-04-03 22:36:24 +0300

    third commits

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python3
-''' Description: Take the code from wait_n and alter it into a new function
-                 task_wait_n. The code is nearly identical to wait_n except
-                 task_wait_random is being called.
-    Arguments: n: int, max_delay: int = 10
-'''
-
-from typing import List
+"""
+4-tasks module
+"""
 import asyncio
-import random
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    '''Execute task_wait_random and returns sorted list of delay'''
-    spawn_ls = []
-    delay_ls = []
-    for i in range(n):
-        delayed_task = task_wait_random(max_delay)
-        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
-        spawn_ls.append(delayed_task)
-    for spawn in spawn_ls:
-        await spawn
-
-    return delay_ls
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    returns the list of all delays in ascending order
+    """
+    c_routines = [task_wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(c_routines)]
