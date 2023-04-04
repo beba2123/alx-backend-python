750861bd4437c885fc86a728602195ebe0221e27
beba2123 <antenehbeba@gmail.com>
2023-04-04 15:43:46 +0300

    async commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,9 +10,9 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     ''' for generating a sequence of 10 numbers.'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
-        
