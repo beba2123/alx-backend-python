c3e1ac737abf704ce5dcca74403ce63e9682cd00
beba2123 <antenehbeba@gmail.com>
2023-04-04 15:41:32 +0300

    async commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+file: 0-async_generator.py
+Desc: A python module that contains codes related to async
+      comprhensions.
+Author: Anteneh Alem
+Date Created: April 04, 2023
+"""
+import asyncio
+import random
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    ''' for generating a sequence of 10 numbers.'''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
+        
